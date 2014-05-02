f50772efe352c4f9e4d150cfa4b21efdd9d0ca2d
Damien DUPORTAL <damien.duportal@gmail.com>
2014-05-02 08:44:22 +0200

    HAProxy with vagrant : provisionning scripts are working now

--- haproxy/vagrant/install_lb.sh
+++ haproxy/vagrant/install_lb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ### Adding backports repository
+echo "== Cheking and installing all missing stuff"
 sed -i '/backports/d' /etc/apt/sources.list
 echo "deb http://ftp.debian.org/debian/ wheezy-backports main" >> /etc/apt/sources.list
 apt-get update
@@ -9,13 +10,14 @@ apt-get update
 apt-get install -y keepalived vim curl libnet-ifconfig-wrapper-perl haproxy vim-haproxy
 
 ### Configure keepalive
+echo "== Configuring Keepalive"
 update-rc.d keepalived defaults
 if [[ $(/sbin/ifconfig | grep 'inet addr' | grep 192.168.0.11 | wc -l) -ne 0 ]]; then
 	KEEPALIVE_PRIORITY=101 # We are master
 else
 	KEEPALIVE_PRIORITY=100 # We are backup
 fi
-cat >> /etc/keepalived/keepalived.conf << EOF
+cat > /etc/keepalived/keepalived.conf << EOF
 
 vrrp_script chk_haproxy {
 	script "killall -0 haproxy"     # cheaper than pidof
@@ -39,8 +41,32 @@ EOF
 
 
 ### Configure HAProxy
+echo "== Configurint HA Proxy"
 sed -i 's/ENABLED=./ENABLED=1/g' /etc/default/haproxy 
-cat >> /etc/haproxy/haproxy.cfg << EOF
+cat > /etc/haproxy/haproxy.cfg << EOF
+global
+        log /dev/log    local0
+        log /dev/log    local1 notice
+        chroot /var/lib/haproxy
+        user haproxy
+        group haproxy
+        daemon
+
+defaults
+        log     global
+        mode    http
+        option  httplog
+        option  dontlognull
+        contimeout 5000
+        clitimeout 50000
+        srvtimeout 50000
+        errorfile 400 /etc/haproxy/errors/400.http
+        errorfile 403 /etc/haproxy/errors/403.http
+        errorfile 408 /etc/haproxy/errors/408.http
+        errorfile 500 /etc/haproxy/errors/500.http
+        errorfile 502 /etc/haproxy/errors/502.http
+        errorfile 503 /etc/haproxy/errors/503.http
+        errorfile 504 /etc/haproxy/errors/504.http
 
 listen webfarm *:80
        mode http
@@ -57,10 +83,15 @@ EOF
 
 
 ### Configuring and loading kernel parameters
+echo "== Configuring Kernel"
 sed -i '/ipv4.ip_forward/d' /etc/sysctl.conf
 sed -i '/ipv4.ip_nonlocal_bind/d' /etc/sysctl.conf
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 echo "net.ipv4.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
 sysctl -p
 
+### GO !
+echo "== Startin' services"
+service haproxy start
+service keepalived start
 
--- haproxy/vagrant/install_nginx.sh
+++ haproxy/vagrant/install_nginx.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Installing stuff
+echo "== Checkin' and installin' some needed stuff"
 sed -i '/nginx/d' /etc/apt/sources.list
 echo "deb http://nginx.org/packages/debian/ wheezy nginx" >> /etc/apt/sources.list
 echo "deb-src http://nginx.org/packages/debian/ wheezy nginx" >> /etc/apt/sources.list
@@ -11,4 +13,26 @@ rm -f *key
 
 sudo apt-get update
 sudo apt-get install -y nginx vim libnet-ifconfig-wrapper-perl
+
+echo "== Adding nginx content"
+cat > /usr/share/nginx/html/check.txt << EOF
+OK
+EOF
+
+MY_SERVICE_IP=$(/sbin/ifconfig | grep eth1 -A2 | grep 'inet addr' | awk '{print $2}' | cut -d':' -f2)
+
+cat > /usr/share/nginx/html/index.html << EOF
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx!</title>
+</head>
+<body>
+<h1>Welcome to nginx!</h1>
+<p>
+My IP is $MY_SERVICE_IP
+</p>
+</body>
+</html>
+EOF
 
